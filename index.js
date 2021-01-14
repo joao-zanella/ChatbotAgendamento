3d638afc2fe20adae5076aff69ebfb3ae13bd1d1
Lucas <telltaledias@gmail.com>
2021-01-14 12:05:21 -0300

    Implementando Horários

--- index.js
+++ index.js
@@ -313,7 +313,7 @@ async function handleMessage(sender_psid, received_message) {
                 }
             ]
         };
-    } else if (received_message.text == '14/01/2021' || '15/01/2021') {
+    } else if (received_message.payload == '14/01/2021' || '15/01/2021') {
 
         const pegaHoras = await horariosLivresDiaEspecifico(received_message.text);
         console.log(pegaHoras.horas);
