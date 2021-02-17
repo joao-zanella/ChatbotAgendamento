6a58decdd5fae147d096108ecbe6e918013fa23a
Splora <azure@zanella.inf.br>
2021-02-17 15:45:03 -0300

    reiniciando

--- index.js
+++ index.js
@@ -4,35 +4,53 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
+  request = require('request');
   app = express().use(bodyParser.json()); // creates express http server
 
 
-  const PAGE_ACCESS_TOKEN="EAAellojyLaEBANwWJbojFpjbLn3ycWKG7ZBKVmNHeQnQJbtV3ybs3JeD1s35PaiDQWJCGpnIJZBaBdEiW9qqcxlnebUil4wZBBkFmbxpZBzbgmqZAnzPhdzuRhj9hNH0lqvZBQkXxZC18Kcbgr9LGl3mcPnRIJwfxGWNEspEHXhl9QcmkbETltpurWQLmZCk7r8ZD"
+  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+  console.log(PAGE_ACCESS_TOKEN);
+  
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
- 
+
+    // Parse the request body from the POST
     let body = req.body;
   
-    // Checks this is an event from a page subscription
+    // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
   
-      // Iterates over each entry - there may be multiple if batched
+      // Iterate over each entry - there may be multiple if batched
       body.entry.forEach(function(entry) {
-  
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
+
+        // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+      
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+
+
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+        }
+      
       });
   
-      // Returns a '200 OK' response to all requests
+      // Return a '200 OK' response to all events
       res.status(200).send('EVENT_RECEIVED');
+  
     } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
+      // Return a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
     }
   
@@ -42,7 +60,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "QWERTY"
+    let VERIFY_TOKEN = "QWERTY";
       
     // Parse the query params
     let mode = req.query['hub.mode'];
@@ -65,6 +83,107 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+    let response;
+  
+    // Checks if the message contains text
+    if (received_message.text) {    
+      // Create the payload for a basic text message, which
+      // will be added to the body of our request to the Send API
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      }
+    } else if (received_message.attachments) {
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Is this the right picture?",
+              "subtitle": "Tap a button to answer.",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+    } 
+    
+    // Send the response message
+    callSendAPI(sender_psid, response);
+  }
+
+function handlePostback(sender_psid, received_postback) {
+    let response;
+    
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+  
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+  }
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+        console.log('message sent!')
+        } else {
+        console.error("Unable to send message:" + err);
+        }
+    }); 
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 'use strict';
 // const
